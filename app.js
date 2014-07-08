482cbb81fe2d70026b5ecf6af18ee3a959a80184
Xianhui Feng <xianhui.feng100@gmail.com>
2014-07-08 11:54:33 -0700

    added another page

--- app.js
+++ app.js
@@ -1,56 +1,75 @@
 'use strict';
 var app = angular.module('af', ['ngRoute']);
 app.selectedData = [];
-app.controller('listController', function($scope) {
-
-	$scope.tops = [
+app.tops = [
     {
-    	'id': '1',
+    	'id': 'top1',
     	'img': 'http://anf.scene7.com/is/image/anf/anf_79816_02_prod1?$category-anf$',
     	'name': 'Blythe Snit',
       'desc': 'New Arrival',
  	 		'price': '$28'
  	 	},
     {
-    	'id': '2',
+    	'id': 'top2',
     	'img': "http://anf.scene7.com/is/image/anf/anf_78777_02_prod1?$category-anf$",
     	'name': 'Hadley Tank',
       'desc': 'Flagship Exclusvie',
  	 		'price': '$48'
  	 	},
     {
-    	'id': '3',
+    	'id': 'top3',
     	'img': "http://anf.scene7.com/is/image/anf/anf_78742_02_prod1?$category-anf$",
     	'name': 'Jude Tank',
       'desc': 'Online Exclusive & New Arrival',
  	 		'price': '$20'
  	 	},
-  ];
-
-  $scope.bottoms = [
+];
+app.bottoms = [
  	 	{
- 	 		'id': '4',
+ 	 		'id': 'bot1',
     	'img': 'http://anf.scene7.com/is/image/anf/anf_76944_01_prod1?$category-anf$',
     	'name': 'A&F High Rise Short-Shorts',
       'desc': 'Flagship Exclusvie',
  	 		'price': '$29'
  	 	},
     {
-    	'id': '5',
+    	'id': 'bot2',
     	'img': "http://anf.scene7.com/is/image/anf/anf_76349_02_prod1?$category-anf$",
     	'name': 'Savannah Skirt',
       'desc': 'New Arrival',
  	 		'price': '$40.56'
  	 	},
     {
-    	'id': '6',
+    	'id': 'bot3',
     	'img': "http://anf.scene7.com/is/image/anf/anf_79277_01_prod1?$category-anf$",
     	'name': 'Quinn Skirt',
       'desc': 'New Arrival',
  	 		'price': '$32'
  	 	}
-  ];
+];
+app.models = [
+	{
+		'id': 'model1',
+		'img': 'http://anf.scene7.com/is/image/anf/anf_lg_120790?$keyLooksCategory-anf$'
+	},
+	{
+		'id': 'model2',
+		'img': 'http://anf.scene7.com/is/image/anf/anf_lg_12842?$keyLooksCategory-anf$'
+	},
+	{
+		'id': 'model3',
+		'img': 'http://anf.scene7.com/is/image/anf/anf_lg_120786?$keyLooksCategory-anf$'
+	},
+	{
+		'id': 'model4',
+		'img': 'http://anf.scene7.com/is/image/anf/anf_lg_58970?$keyLooksCategory-anf$'
+	}
+];
+	
+app.controller('listController', function($scope) {
 
+	$scope.tops = app.tops;
+  $scope.bottoms = app.bottoms;
   $scope.count = 0;
   $scope.selectedData = app.selectedData;
   $scope.add = function (selected) {
@@ -65,15 +84,21 @@ app.controller('listController', function($scope) {
 		}
   };
 
-  $scope.match = function (data) {
-  	alert("matching");
-  	console.log('data of matching', data);
-  };
 });
 
-app.controller('closetController', function($scope){
-	$scope.test = "Hello closet";
+app.controller('fittingRoomController', function($scope){
+	$scope.selectedData = app.selectedData;
+	$scope.selectedDataImg = [];
+	$scope.models = app.models;
+
+	 $scope.match = function (data) {
+  	for (var i = 0; i < $scope.selectedData.length; i++) {
+  		$scope.selectedDataImg.push($scope.selectedData[i]['img']);
+  	};
+  };
+
 });
+
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
@@ -81,11 +106,11 @@ app.config(['$routeProvider',
         templateUrl: 'main.html',
         controller: 'listController'
       }).
-      when('/closet', {
-        templateUrl: 'closet.html',
-        controller: 'closetController'
+      when('/fittingRoom', {
+        templateUrl: 'fittingRoom.html',
+        controller: 'fittingRoomController'
       }).
       otherwise({
         redirectTo: '/main'
       });
-  }]);
+	}]);
