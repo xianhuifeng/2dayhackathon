76981b31c5493d163dddb34a689b64377fec8a58
Xianhui Feng <xianhui.feng100@gmail.com>
2014-07-07 20:38:00 -0700

    add pictures on index page

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ app.controller('listController', function($scope) {
 
 	$scope.tops = [
     {
+    	'img': 'http://anf.scene7.com/is/image/anf/anf_79816_02_prod1?$category-anf$',
     	'name': 'Blythe Snit',
       'desc': 'New Arrival',
  	 		'price': '$28'
  	 	},
     {
+    	'img': "http://anf.scene7.com/is/image/anf/anf_78777_02_prod1?$category-anf$",
     	'name': 'Hadley Tank',
       'desc': 'Flagship Exclusvie',
  	 		'price': '$48'
  	 	},
     {
+    	'img': "http://anf.scene7.com/is/image/anf/anf_78742_02_prod1?$category-anf$",
     	'name': 'Jude Tank',
       'desc': 'Online Exclusive & New Arrival',
  	 		'price': '$20'
