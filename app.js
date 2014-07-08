93804a48f0c9826db620caecc9e3c81e42a354b0
Xianhui Feng <xianhui.feng100@gmail.com>
2014-07-07 21:17:43 -0700

    arrange design and add shopping bag feature

--- app.js
+++ app.js
@@ -20,11 +20,38 @@ app.controller('listController', function($scope) {
     	'name': 'Jude Tank',
       'desc': 'Online Exclusive & New Arrival',
  	 		'price': '$20'
+ 	 	},
+  ];
+
+  $scope.bottoms = [
+ 	 	{
+    	'img': 'http://anf.scene7.com/is/image/anf/anf_76944_01_prod1?$category-anf$',
+    	'name': 'A&F High Rise Short-Shorts',
+      'desc': 'Flagship Exclusvie',
+ 	 		'price': '$29'
+ 	 	},
+    {
+    	'img': "http://anf.scene7.com/is/image/anf/anf_76349_02_prod1?$category-anf$",
+    	'name': 'Savannah Skirt',
+      'desc': 'New Arrival',
+ 	 		'price': '$40.56'
+ 	 	},
+    {
+    	'img': "http://anf.scene7.com/is/image/anf/anf_79277_01_prod1?$category-anf$",
+    	'name': 'Quinn Skirt',
+      'desc': 'New Arrival',
+ 	 		'price': '$32'
  	 	}
   ];
 
-  $scope.add = function () {
-  	alert('Added to your cart already!');
+
+  $scope.count = 0;
+  $scope.add = function ($event) {
+  	// alert('Added to your cart already!');
+  	$scope.count += 1;
+  	//add to shoppingBag 
+  	console.log('target',$event.target);
+
   };
 
   $scope.match = function () {
